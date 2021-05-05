bdcbcea8be4a44a2449f926b131f02aeb77c98b0
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-05-05 22:25:26 +0200

    Update index.html and main.js

--- main.js
+++ main.js
@@ -1,29 +1,18 @@
 /**
  * Main app module.
  */
-import PButton from "primevue/button";
+import Button from "primevue/button";
 import { createApp } from "vue";
 
 const app = createApp({
   components: {
-    PButton
+    'p-button': Button
   },
   data() {
     return {
       name: "developer",
     };
   },
-  template: `
-    <h2>Greeting</h2>
-    <p>Hello, {{ name }}!</p>
-
-    <PButton label="Submit"></PButton>
-
-    <br>
-    <br>
-
-    <PButton label="Submit" icon="pi pi-check" iconPos="right"></PButton>
-  `,
 });
 
 app.mount("#app");
