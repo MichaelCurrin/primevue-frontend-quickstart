55ac236095eba4697552c278ad15cc998cc2bc9b
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-05-05 22:07:01 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 /**
  * Main app module.
  */
-import { createApp } from "https://unpkg.com/vue@3.0.7/dist/vue.esm-browser.js";
+import PButton from "primevue/button";
+import { createApp } from "vue";
 
 const app = createApp({
   components: {
+    PButton
   },
   data() {
     return {
@@ -14,6 +16,13 @@ const app = createApp({
   template: `
     <h2>Greeting</h2>
     <p>Hello, {{ name }}!</p>
+
+    <PButton label="Submit"></PButton>
+
+    <br>
+    <br>
+
+    <PButton label="Submit" icon="pi pi-check" iconPos="right"></PButton>
   `,
 });
 
