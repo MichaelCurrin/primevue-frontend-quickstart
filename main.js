b3bf00d82f6c4261296150f319b691ba6e3dd84f
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-05-05 22:40:47 +0200

    feat: Add components

--- main.js
+++ main.js
@@ -2,15 +2,20 @@
  * Main app module.
  */
 import Button from "primevue/button";
+import InputText from 'primevue/inputtext';
 import { createApp } from "vue";
 
 const app = createApp({
   components: {
-    'p-button': Button
+    'p-button': Button,
+    'p-inputtext': InputText,
   },
   data() {
     return {
       name: "developer",
+
+      value1: "",
+      value2: "",
     };
   },
 });
