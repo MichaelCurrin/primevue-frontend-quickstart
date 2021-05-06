8298272f2e96901171a5b565dc98d2c081d009c0
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-05-06 10:31:04 +0200

    style: Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,13 @@
  * Main app module.
  */
 import Button from "primevue/button";
-import InputText from 'primevue/inputtext';
+import InputText from "primevue/inputtext";
 import { createApp } from "vue";
 
 const app = createApp({
   components: {
-    'p-button': Button,
-    'p-inputtext': InputText,
+    "p-button": Button,
+    "p-inputtext": InputText,
   },
   data() {
     return {
